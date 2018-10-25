5cf368e3a69c7fb4475bddd4e85096452c9191bc
aidasyaz <aida.syaz24@gmail.com>
2018-10-26 03:35:51 +0800

    recipe features

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,6 @@ class RecipesController < ApplicationController
 
 	def index
 		@recipe = Recipe.all
-
 	end
 
 	def new
@@ -11,7 +10,7 @@ class RecipesController < ApplicationController
 
 	def create
 		@recipe = Recipe.new(recipe_params)
-		
+		@recipe.user_id = current_user.id
 		if @recipe.save
 			flash[:success] = "recipe saved"
 			redirect_to root_path
@@ -23,6 +22,29 @@ class RecipesController < ApplicationController
 
 	def show
 		@recipe = Recipe.find(params[:id])
+	end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def update
+ 		@recipe = Recipe.find(params[:id])
+
+ 		if @recipe.update(recipe_params)
+ 			flash[:success] = "You have successfully edited recipe!"
+			redirect_to recipe_path(@recipe.id)
+ 		else
+ 			flash[:error] = "Recipe couldnt be edited. Try again."
+ 			redirect_to edit_recipe_path(@recipe.id)
+ 		end
+	end
+
+	def destroy
+		@recipe = Recipe.find(params[:id])
+		@recipe.destroy
+
+		redirect_to root_path
 	end
 
 
