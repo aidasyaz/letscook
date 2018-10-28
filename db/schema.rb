fd99ff88298be84e52be1784e869f0aebc557bce
aidasyaz <aida.syaz24@gmail.com>
2018-10-29 00:09:35 +0800

    rspec user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_065538) do
+ActiveRecord::Schema.define(version: 2018_10_28_151332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_10_28_065538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
-    t.string "name"
+    t.string "first_name"
   end
 
   create_table "videos", force: :cascade do |t|
